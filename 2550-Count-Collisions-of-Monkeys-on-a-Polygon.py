9eb06712fef6e90e5ba93351ba15d89546344b35
Nahom Garefo <119704083+Blacknahil@users.noreply.github.com>
2024-09-11 22:32:47 +0300

    [2550] [Time Beats: 54.14%] [Memory Beats: 38.17%] - LeetPush

--- 2550-Count-Collisions-of-Monkeys-on-a-Polygon.py
+++ 2550-Count-Collisions-of-Monkeys-on-a-Polygon.py
@@ -1,22 +1,7 @@
 class Solution:
     def monkeyMove(self, n: int) -> int:
-        visited={}
-        def power(p):
-            if p==0:
-                return 1
-            if p==1:
-                return 2
-            if p in visited:
-                return visited[p]
-            if p%2==0:
-                res=power(p//2)*power(p//2)
-            else:
-                res=2*power(p//2)*power(p//2)
-            res%=(10**9 +7)
-            visited[p]=res
-            return res
-
-        ans=power(n)
-        ans=(ans-2)%(10**9 +7)
-        return ans
+        MOD=10**9 +7
+        total_ways=pow(2,n,MOD)
+        noCollison=2
+        return (total_ways-noCollison)%MOD
         
