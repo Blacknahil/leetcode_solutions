4a58c57d90965c82c076988927b19b20edd112d4
Nahom Garefo <119704083+Blacknahil@users.noreply.github.com>
2024-09-13 16:45:18 +0300

    [1310] [Time Beats: 70.79%] [Memory Beats: 34.46%] - LeetPush

---
+++ 1310-XOR-Queries-of-a-Subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        prefix=[0]
+        for num in arr:
+            prefix.append(prefix[-1]^num)
+        ans=[]
+        for left,right in queries:
+            ans.append(prefix[left]^prefix[right+1])
+            
+        return ans
